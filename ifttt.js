6d081b452a05e273204ac6f27d0ef186e7614450
Steve C <redsteve@gmail.com>
2015-09-17 10:55:59 -0400

    added comments for clarity

--- ifttt.js
+++ ifttt.js
@@ -1,13 +1,13 @@
 var config = require('./config.json');
 var arpListener = require('arp-listener');
 var request = require('request');
-
-arpListener('wlan0', function(arpData) {
+// Change the interface to whatever you are listening on - currently wlan0
+arpListener('wlan0', function(arpData) { 
 
   if (arpData.sender_ha == config.pushMAC) {
   	console.log('pushed');
   	request.post({
-      url: 'http://maker.ifttt.com/trigger/glad.dash_button/with/key/' + config.iftttkeys 
+      url: 'http://maker.ifttt.com/trigger/gillette.dash_button/with/key/' + config.iftttkeys 
     }, function(error, response, body) {
       console.log(body);
       console.log('Error was ', error);
